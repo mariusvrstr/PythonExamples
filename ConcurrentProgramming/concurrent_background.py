b75a764f959e88fabc6771ac6e8349e743d0cc7b
Marius Vorster <marius.vrstr@gmail.com>
2021-02-07 22:42:16 +0200

    Including example with main execution after scheduling concurrent background processes

--- ConcurrentProgramming/concurrent_background.py
+++ ConcurrentProgramming/concurrent_background.py
@@ -2,28 +2,33 @@ import time
 import asyncio
 from concurrent.futures import ProcessPoolExecutor
 
-class ConcurrentMethodExample(object):
+class ConcurrentBackgroundExample(object):
 
     async def sample_async_method_a(self):
-        for k in range(3):
+        for k in range(10):
             print(f"Running asyncronous method a - {k}")
             await asyncio.sleep(1)
 
 
     async def sample_async_method_b(self):
-        for k in range(3):            
+        for k in range(10):            
             print(f"Running asyncronous method b - {k}")
             await asyncio.sleep(1)
 
     async def start(self):
-        tasks = [self.sample_async_method_a(), self.sample_async_method_b()]
-        await asyncio.wait(tasks)
+        asyncio.ensure_future(self.sample_async_method_a())        
+        asyncio.ensure_future(self.sample_async_method_b())
+
+        for k in range(10):            
+            print(f"Main menu normal processes - {k}")
+            await asyncio.sleep(1)
+
  
     def run(self):
         print("Start example")
 
         asyncio.run(self.start())
-        
+       
         print("End example")
         pass
 
--- ConcurrentProgramming/main.py
+++ ConcurrentProgramming/main.py
@@ -1,11 +1,13 @@
 from async_basics import AsyncExample
 from concurrent_basic import ConcurrentMethodExample
+from concurrent_background import ConcurrentBackgroundExample
 
 
 # AsyncExample().run()
+# ConcurrentMethodExample().run()
 
-
-ConcurrentMethodExample().run()
+# Working make sure the main thread have context switching break points
+ConcurrentBackgroundExample().run()
 
 
 
