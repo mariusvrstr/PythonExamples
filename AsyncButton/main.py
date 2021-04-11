0d536b66d00d2da7051906da61f634fb65c6a57a
Marius Vorster <marius.vrstr@gmail.com>
2021-04-11 21:57:07 +0200

    Added GUI popup - Asyncio logic not executing

--- AsyncButton/main.py
+++ AsyncButton/main.py
@@ -2,6 +2,8 @@
 from modules.module_one import ModuleOne
 from modules.module_two import ModuleTwo
 from modules.monitoring import Monitoring
+from main_gui import Ui_MainWindow
+
 
 import time
 import asyncio
@@ -11,16 +13,21 @@ module_two = None
 
 counters = []
 
-async def initialize(display_module, module_one, module_two):
+async def initialize(gui_module, display_module, module_one, module_two):
     asyncio.ensure_future(module_one.start_monitoring())
     asyncio.ensure_future(module_two.start_monitoring())
 
-    # Main thread is the display thread
+    # Open GUI Window
+    gui_module.start_gui()
+
+    # TODO: Why does the ASYNCIO only kick in when the GUI is closed? Blokking the main thread?
+    # it seem like all the asyncio is properly registered and starts executing but only if the GUI thread is stopped
     await display_module.start_display(counters)
-    pass
 
 def main():
     display_module =  Monitoring()
+    gui_module = Ui_MainWindow()
+
     module_one = ModuleOne(3)
     module_two = ModuleTwo(8)
 
@@ -30,12 +37,13 @@ def main():
     loop = asyncio.get_event_loop()
 
     try:
-        loop.run_until_complete(initialize(display_module, module_one, module_two))
+        loop.run_until_complete(initialize(gui_module, display_module, module_one, module_two))
 
     except KeyboardInterrupt:
         module_one.dispose()
         module_two.dispose()
         display_module.dispose()
+        gui_module.dispose()
 
 main()
 time.sleep(5)   
